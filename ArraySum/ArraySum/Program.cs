b83ff5eb5daf11c68bf18250458a49b2aeca1373
Lucas Souza <lucasc_souza1@outlook.com>
2022-01-23 16:42:21 -0400

    Arraysum

---
+++ ArraySum/ArraySum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;   
+        for (int i = 0; i < ar.Count; i++)
+        {
+            sum += ar[i];
+        }
+         return sum;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string b = Console.ReadLine();
+        int[] numbers = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+        Console.WriteLine(numbers.Sum());
+
+    }
+}
